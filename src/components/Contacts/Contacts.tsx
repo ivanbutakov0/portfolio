f32567c1f80aa76d2aecfcfe99ab1a27a4b76807
Ivan Butakov <ivanbutakov000@gmail.com>
2024-06-14 13:02:10 +0500

    add link to ru version to contacts

--- src/components/Contacts/Contacts.tsx
+++ src/components/Contacts/Contacts.tsx
@@ -5,6 +5,9 @@ const Contacts = () => {
 	return (
 		<div>
 			<ul className={styles.list}>
+				<li title='ru'>
+					<a href='https://portfolio-ru-ten.vercel.app/'>Ru</a>
+				</li>
 				{contacts.map(contact => (
 					<li title={contact.name} className={styles.item} key={contact.name}>
 						<a href={contact.link} target='_blank'>
